a6d66efdc0fab0d17c3ca88879f5a47d819c1acf
Aman Ahmed <aman.ahmed97@gmail.com>
2022-09-29 13:07:59 -0400

    Almost done OrderNChaos

--- Java-TicTacToe-Game/src/Main.java
+++ Java-TicTacToe-Game/src/Main.java
@@ -193,8 +193,8 @@ class RunTicTacToe{
             try{
                 turnInput = ip.nextInt();
             }catch (Exception e){
-                turnInput=0;
-                break;
+                // Flush the input token, to ask input again
+                ip.next();
             }
 
             if(SymbolTurn==TTTSymbol.symbolX && turnInput>=1 && turnInput<=(board.rows*board.columns) &&
@@ -226,26 +226,148 @@ class RunTicTacToe{
     }
 }
 
+class RunOrderNChaos{
+
+    public static boolean gameCondition(BoardTicTacToe board){
+        int countXRow = 0;
+        int countXColumn = 0;
+        int countXDiagonal1 = 0;
+        int countXDiagonal2 = 0;
+        int countORow = 0;
+        int countOColumn = 0;
+        int countODiagonal1 = 0;
+        int countODiagonal2 = 0;
+
+        for(int i=0;i<board.rows;i++){
+            countXRow = 0;
+            countXColumn = 0;
+            countXDiagonal1 = 0;
+            countXDiagonal2 = 0;
+            countORow = 0;
+            countOColumn = 0;
+            countODiagonal1 = 0;
+            countODiagonal2 = 0;
+            for(int j=0;j<board.rows;j++){
+                // For X
+                if(board.getBoardSymbol(i*board.rows+j) == TTTSymbol.symbolX)
+                    countXRow++;
+                if(board.getBoardSymbol(j*board.rows+i) == TTTSymbol.symbolX)
+                    countXColumn++;
+                if(board.getBoardSymbol(j*board.rows+j) == TTTSymbol.symbolX)
+                    countXDiagonal1++;
+                if(board.getBoardSymbol((j*board.rows+(board.rows-1-j))) == TTTSymbol.symbolX)
+                    countXDiagonal2++;
+                // For O
+                if(board.getBoardSymbol(i*board.rows+j) == TTTSymbol.symbolO)
+                    countORow++;
+                if(board.getBoardSymbol(j*board.rows+i) == TTTSymbol.symbolO)
+                    countOColumn++;
+                if(board.getBoardSymbol(j*board.rows+j) == TTTSymbol.symbolO)
+                    countODiagonal1++;
+                if(board.getBoardSymbol((j*board.rows+(board.rows-1-j))) == TTTSymbol.symbolO)
+                    countODiagonal2++;
+            }
+            if(countXRow>=5 || countXColumn>=5 || countXDiagonal1>=5 || countXDiagonal2>=5){
+                System.out.println("X Line Found!!\nOrder Wins!!");
+                return true;
+            }
+            if(countORow>=5 || countOColumn>=5 || countODiagonal1>=5 || countODiagonal2>=5){
+                System.out.println("O Line Found!!\nOrder Wins!!");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void runGame(BoardTicTacToe board){
+        System.out.println("Starting game");
+        int gameTurn = 0;
+        int turnInput = 0;
+        Scanner ip = new Scanner(System.in);
+        char SymbolTurn = TTTSymbol.symbolX;
+
+        while(gameTurn<(board.rows*board.columns)){
+            board.printBoard();
+            try{
+                System.out.print("Choose "+TTTSymbol.symbolX+" or "+TTTSymbol.symbolO+" to mark on board : ");
+                SymbolTurn = ip.next().charAt(0);
+                System.out.print("Enter a position on board to mark it : ");
+                turnInput = ip.nextInt();
+            }catch (Exception e){
+                // Flush the input token, to ask input again
+                ip.next();
+            }
+
+            if(SymbolTurn==TTTSymbol.symbolX && turnInput>=1 && turnInput<=(board.rows*board.columns) &&
+                    board.getBoardSymbol(turnInput-1)=='-'){
+                board.setBoard(turnInput-1, TTTSymbol.symbolX);
+                gameTurn++;
+                SymbolTurn = TTTSymbol.symbolO;
+            }
+            else if(SymbolTurn==TTTSymbol.symbolO && turnInput>=1 && turnInput<=(board.rows*board.columns) &&
+                    board.getBoardSymbol(turnInput-1)=='-'){
+                board.setBoard(turnInput-1, TTTSymbol.symbolO);
+                gameTurn++;
+                SymbolTurn = TTTSymbol.symbolX;
+            }
+            else
+                System.out.println("Enter valid position. Try Again.");
+
+            if(RunOrderNChaos.gameCondition(board)){
+                board.printBoard();
+                System.out.println("Game Won!!");
+                break;
+            }
+        }
+
+        if(!gameCondition(board)){
+            board.printBoard();
+            System.out.println("Chaos Wins!!\nGame Over!");
+        }
+    }
+}
+
 public class Main {
     public static void playerSet(){
-        Player player1, player2;
+        int numberPlayers = 2;
         Scanner ip = new Scanner(System.in);
-        // Player 1
-        System.out.println("Player 1:");
-        System.out.print("Please enter your name:");
-        player1 = new Player(ip.nextLine(),1);
-        System.out.println("Hello "+player1.name+"!\n");
-        // Player 2
-        System.out.println("Player 2:");
-        System.out.print("Please enter your name:");
-        player2 = new Player(ip.nextLine(),2);
-        System.out.println("Hello "+player2.name+"!\n");
+        System.out.print("Enter number of players : ");
+        try {
+            numberPlayers = ip.nextInt();
+        }catch(InputMismatchException e){
+            // Flush the input token, to ask input again
+            ip.next();
+            System.out.print("Invalid non-integer input.\nEnter number of players : ");
+            numberPlayers = ip.nextInt();
+        }
+        Player[] players = new Player[numberPlayers];
+        ip.nextLine();
+
+        for(int i=0; i<numberPlayers;i++){
+            // Player Information input
+            System.out.println("Player "+(i+1)+" :");
+            System.out.print("Please enter your name:");
+            players[i] = new Player(ip.nextLine(),i+1);
+            System.out.println("Hello "+players[i].name+"!\n");
+        }
+//        // Player 1
+//        System.out.println("Player 1:");
+//        System.out.print("Please enter your name:");
+//        player1 = new Player(ip.nextLine(),1);
+//        System.out.println("Hello "+player1.name+"!\n");
+//        // Player 2
+//        System.out.println("Player 2:");
+//        System.out.print("Please enter your name:");
+//        player2 = new Player(ip.nextLine(),2);
+//        System.out.println("Hello "+player2.name+"!\n");
     }
 
     public static void main(String[] args) {
         //Initialize the objects of the game
         Scanner ip = new Scanner(System.in);
         BoardTicTacToe board;
+        int option = 0;
 //        board.printBoard();
 
         // Input for the game
@@ -258,12 +380,21 @@ public class Main {
             System.out.println("\nSelect your game:\n1.Tic-Tac-Toe\n2.Order and Chaos\n3.Exit");
             System.out.print("Enter:");
 
-            int option = ip.nextInt();
+            try{
+                option = ip.nextInt();
+            }catch(Exception e){
+                // Flush the input token, to ask input again
+                ip.next();
+            }
 
             switch (option){
                 case 1:
                     board = new BoardTicTacToe(3);
                     RunTicTacToe.runGame(board);
+                    break;
+                case 2:
+                    board = new BoardTicTacToe(6);
+                    RunOrderNChaos.runGame(board);
                     break;
                 case 3:
                     System.exit(0);
